9cb7e7d7ee7ee44ffb7b6a71b36441b3a37a0564
omarieclaire <marieflanagan@gmail.com>
2018-05-05 20:40:51 -0400

    add modals

--- public/d3/js/main.js
+++ public/d3/js/main.js
@@ -1,1 +1,33 @@
+var createModal = function(modalId, buttonId, spanIndex) {
+	// Get the modal
+	var modal = document.getElementById(modalId);
 
+	// Get the button that opens the modal
+	var btn = document.getElementById(buttonId);
+
+	// Get the <span> element that closes the modal
+	var span = document.getElementsByClassName("close")[spanIndex];
+
+
+	// When the user clicks on the button, open the modal 
+	btn.onclick = function() {
+	    modal.style.display = "block";
+	}
+
+	// When the user clicks on <span> (x), close the modal
+	span.onclick = function() {
+	    modal.style.display = "none";
+	}
+
+	// When the user clicks anywhere outside of the modal, close it
+	//window.onclick = function(event) {
+	window.addEventListener('click', function(event) {
+	    if (event.target == modal) {
+	        modal.style.display = "none";
+	    }
+	});
+}
+
+createModal('aboutModal', 'aboutBtn', 0);
+createModal('joinModal', 'joinBtn', 1);
+createModal('settingsModal', 'settingsBtn', 2);
