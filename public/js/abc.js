41133e1a858909af3dc2381f8de8c5b1774cda98
Erik Osheim <d_m@plastic-idolatry.com>
2018-05-11 02:16:22 -0400

    abc.html is a demo that sort of works

---
+++ public/js/abc.js
@@ -0,0 +1,147 @@
+var svg = d3.select("svg"),
+    width = +svg.attr("width"),
+    height = +svg.attr("height"),
+    color = d3.scaleOrdinal(d3.schemeCategory10);
+
+var seenNodes = {};
+var seenEdges = {};
+
+var nodes = [];
+var links = [];
+
+function edgeId(from, to) {
+  if (from < to) {
+    return from + "-" + to;
+  } else {
+    return to + "-" + from;
+  }
+}
+
+addEdge("a", "b");
+addEdge("b", "c");
+addEdge("d", "e");
+addEdge("d", "f");
+addEdge("d", "g");
+addEdge("e", "h");
+addEdge("h", "i");
+addEdge("h", "b");
+addEdge("i", "a");
+addEdge("i", "b");
+addEdge("i", "c");
+addEdge("i", "d");
+
+function addNode(id) {
+  if (seenNodes[id]) {
+    return seenNodes[id];
+  } else {
+    var o = {id: id};
+    nodes.push(o);
+    //console.log("added node %o", o);
+    seenNodes[id] = o;
+    return o;
+  }
+}
+
+function addEdge(from, to) {
+  var id = edgeId(from, to);
+  if (from === to) {
+    // do nothing
+  } else if (seenEdges[id]) {
+    //console.log("edge %o -> %o already exists", from, to);
+  } else {
+    var x = addNode(from);
+    var y = addNode(to);
+    var o = {source: x, target: y};
+    links.push(o);
+    seenEdges[id] = 1;
+    //console.log("created edge %o", o);
+  }
+}
+
+var simulation = d3.forceSimulation(nodes)
+      .force("charge", d3.forceManyBody().strength(-100))
+      .force("link", d3.forceLink(links).distance(100))
+      .force("center", d3.forceCenter())
+      .force("collide", d3.forceCollide(40))
+      .force("x", d3.forceX())
+      .force("y", d3.forceY())
+      .alphaTarget(0)
+      .on("tick", ticked);
+
+var g = svg
+      .append("g")
+      .attr("transform", "translate(" + width / 2 + "," + height / 2 + ")");
+
+var link = g
+      .append("g")
+      .attr("stroke", "#000")
+      .attr("stroke-width", 1.5)
+      .selectAll(".link");
+
+var node = g
+      .append("g")
+      .attr("stroke", "#fff")
+      .attr("stroke-width", 1.5)
+      .selectAll(".node");
+
+var label = g
+      .append("g")
+      .attr("stroke", "#fff")
+      .attr("stroke-width", 1.5)
+      .selectAll(".node");
+
+restart();
+
+function restart() {
+  // Apply the general update pattern to the nodes.
+  node = node.data(nodes, function(d) { return d.id;});
+  node.exit().remove();
+  node = node.enter()
+    .append("circle")
+    .attr("fill", function(d) { return color(d.id) })
+    .attr("r", 8)
+    .merge(node);
+
+  label = label.data(nodes, function(d) { return d.id;});
+  label.exit().remove();
+  label = label.enter()
+    .append("text")
+    .text(function(d) { return d.id })
+    .style("fill", "#000000")
+    .style("stroke", "#000000")
+    .merge(label);
+
+  // Apply the general update pattern to the links.
+  link = link.data(links, function(d) { return d.source.id + "-" + d.target.id; });
+  link.exit().remove();
+  link = link.enter().append("line").merge(link);
+
+  // Update and restart the simulation.
+  simulation.nodes(nodes);
+  simulation.force("link").links(links);
+  simulation.alpha(1).restart();
+}
+
+function ticked() {
+  node
+    .attr("cx", function(d) { return d.x; })
+    .attr("cy", function(d) { return d.y; })
+  link
+    .attr("x1", function(d) { return d.source.x; })
+    .attr("y1", function(d) { return d.source.y; })
+    .attr("x2", function(d) { return d.target.x; })
+    .attr("y2", function(d) { return d.target.y; });
+  label
+    .attr("x", function(d){ return d.x + 5; })
+    .attr("y", function(d) {return d.y - 5; });
+}
+
+window.onload = function () {
+  document.getElementById("add").addEventListener('click', function () {
+    var from = document.getElementById("from").value;
+    var to = document.getElementById("to").value;
+    //console.log("click %o %o", from, to);
+    addEdge(from, to);
+    restart();
+  });
+};
