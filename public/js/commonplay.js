d0107e6c390b46ed3c2b0dbdb017773efa2d2fe1
omarieclaire <marieflanagan@gmail.com>
2018-05-21 12:59:38 -0400

    Custom colors and fix force

--- public/js/commonplay.js
+++ public/js/commonplay.js
@@ -7,8 +7,9 @@ var svg = d3.select("svg");
 var svgWidth = +svg.attr("width");
 var svgHeight = +svg.attr("height");
 
-var colorPicker = d3.scaleOrdinal(d3.schemeCategory10);
-//var colorPicker = d3.scaleOrdinal.range(["#A07A19", "#AC30C0", "#EB9A72", "#BA86F5", "#EA22A8"]);
+//var colorPicker = d3.scaleOrdinal(d3.schemeCategory10);
+//console.log(d3.schemeCategory10);
+var colorPicker = d3.scaleOrdinal(["#A07A19", "#AC30C0", "#EB9A72", "#BA86F5", "#EA22A8"]);
 
 
 // set of nodes/edges we have already seen (objects)
@@ -46,7 +47,7 @@ var simulation = d3.forceSimulation(nodes)
 	//for making elements attract or repel one another
 	.force("charge", d3.forceManyBody().strength(20))
 	//for creating a fixed distance between connected elements
-	.force("link", d3.forceLink(edges).distance(-600))
+	.force("link", d3.forceLink(edges).distance(100))
 	//for setting the center of gravity of the system
 	.force("center", d3.forceCenter(50))
 	//for preventing elements overlapping
