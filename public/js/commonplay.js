09fd9d98d2b77e22d89c1eaecb0a9c48d853c9ad
omarieclaire <marieflanagan@gmail.com>
2018-05-22 15:25:07 -0400

    First JS button

--- public/js/commonplay.js
+++ public/js/commonplay.js
@@ -362,5 +362,41 @@ window.onload = function() {
 		svg.transition().duration(500).call(zoomCall.scaleTo, currentScale - ZOOM_AMOUNT).transition();
 	});
 
+//button
+  var button = document.getElementById("button");
+  button.addEventListener("click", confirm(button), false);
+
+  function confirm(button) {
+    var className = button.getAttribute("class"),
+        html = button.innerHTML,
+        then,
+        state = 0;
+
+    button.addEventListener("mouseout", function() {
+      if (state == 1) cancel();
+    }, false);
+
+    function cancel() {
+      state = 0;
+      button.setAttribute("class", className);
+      button.innerHTML = html;
+    }
+
+    return function() {
+      if (state) {
+        if (new Date() - then > 500) {
+          state = 2; // launched
+          button.innerHTML = "Missile launched!";
+          setTimeout(cancel, 5000); // reset
+        }
+      } else {
+        state = 1; // confirming
+        then = +new Date();
+        button.setAttribute("class", className + " confirm");
+        button.innerHTML = "Are you sure?";
+      }
+    };
+  }
+
   state.loaded = true;
 };
