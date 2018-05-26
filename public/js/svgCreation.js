f64161b0dbe058a08dc33008c283015230d8ac5b
omarieclaire <marieflanagan@gmail.com>
2018-05-26 09:36:08 -0400

    reformat svgCreation.js file

--- public/js/svgCreation.js
+++ public/js/svgCreation.js
@@ -4,20 +4,20 @@ var svg = d3.select(chart).append("svg");
 var svgWidth = 0;
 var svgHeight = 0;
 
-      function redraw(){
+function redraw() {
 
-        // Extract the width and height that was computed by CSS.
-        var width = chart.clientWidth;
-        var height = chart.clientHeight;
+  // Extract the width and height that was computed by CSS.
+  var width = chart.clientWidth;
+  var height = chart.clientHeight;
 
-        // Use the extracted size to set the size of an SVG element.
-        svg
-          .attr("width", width)
-          .attr("height", height);
+  // Use the extracted size to set the size of an SVG element.
+  svg
+    .attr("width", width)
+    .attr("height", height);
 
-          svgWidth = +svg.attr("width");
-          svgHeight = +svg.attr("height");
-        }
+  svgWidth = +svg.attr("width");
+  svgHeight = +svg.attr("height");
+}
 
 
 // attributes of the svg canvas as variables
@@ -26,7 +26,7 @@ var svgHeight = 0;
 // console.log(svgWidth);
 
 // Draw for the first time to initialize.
-      redraw();
+redraw();
 
-      // Redraw based on the new size whenever the browser window is resized.
-      window.addEventListener("resize", redraw);
+// Redraw based on the new size whenever the browser window is resized.
+window.addEventListener("resize", redraw);
