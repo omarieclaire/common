6301c1623ba9e7914a0ac78f3cfcfd611ea15ca8
omarieclaire <marieflanagan@gmail.com>
2018-05-26 20:33:19 -0400

    minigame failed attempt 2

--- public/js/minigame.js
+++ public/js/minigame.js
@@ -1,4 +1,3 @@
-var colors;
 var screenW = window.innerWidth;
 var screenH = window.innerHeight;
 var buttonWidth = 100;
@@ -8,8 +7,12 @@ var rightButton;
 
 var leftButtonX = 50;
 var leftButtonY = screenH/2 - buttonWidth/2;
-var rightButtonX = screenW - button - 50;
+var rightButtonX = screenW - (buttonWidth + 50);
 var rightButtonY = screenH/2 - buttonWidth/2;
+
+// p1bounds = font.textBounds(message,x,y,fontsize);
+
+
 var leftButtonBar;
 var rightButtonBar;
 var leftBarWidth = 50;
@@ -19,21 +22,28 @@ var rightBarWidth = 50;
 function setup() {
   createCanvas(screenW, screenH);
   //background(200);
+}
 
-  // leftButton = createSprite(leftButtonX, leftButtonY, buttonWidth, buttonWidth);
-  // rightButton = createSprite(rightButtonX, rightButtonY, buttonWidth, buttonWidth);
-  // leftButtonBar = createSprite(leftButtonX + buttonWidth, leftButtonY+buttonWidth, 10, 5);
-  // rightButtonBar = createSprite(leftButtonX, leftButtonY+buttonWidth, 10, 5);
-  // leftButtonBar.visible = true;
-  // rightButtonBar.visible = true;
-
-  // An array of five colors, one for each finger
-  // colors = [color(255,0,0), color(0,255,0), color(0,0,255), color(255, 255,0), color(0,255,255)];
+function withinXLeftBoundary(t) {
+  return t.x > leftButtonX && t.x < leftButtonX + buttonWidth;
+}
+function withinYLeftBoundary(t) {
+  return t.y > buttonY && t.y < buttonY + buttonWidth;
+}
+function withinXRightBoundary(t) {
+  return t.x > rightButtonX && t.x < rightButtonX + buttonWidth;
+}
+function withinYRightBoundary(t) {
+  return t.y > buttonY && t.y < buttonY + buttonWidth;
+}
+function withinLeft(t) {
+  return withinXLeftBoundary(t) && withinYLeftBoundary(t);
+}
+function withinRight(t) {
+  return withinXRightBoundary(t) && withinYRightBoundary(t);
 }
 
 function draw() {
-  // background(black);
-
   fill(0);
   strokeWeight(2);
 
@@ -41,49 +51,70 @@ function draw() {
 
   rect(leftButtonX, leftButtonY, buttonWidth, buttonWidth);
   rect(rightButtonX, rightButtonY, buttonWidth, buttonWidth);
-  rect(leftButtonX + buttonWidth, leftButtonY + buttonWidth / 2, leftBarWidth, 50);
-  rect(rightButtonX - rightBarWidth, rightButtonY + buttonWidth /2, rightBarWidth, 50);
+  // rect(leftButtonX + buttonWidth, leftButtonY + buttonWidth / 2, leftBarWidth, 50);
+  // rect(rightButtonX - rightBarWidth, rightButtonY + buttonWidth /2, rightBarWidth, 50);
 
   // The touches array holds an object for each and every touch
   // The array length is dynamic and tied to the number of fingers
   // currently touching
 
-  for (var i = 0; i < touches.length; i++) {
-    noStroke();
-    if ((touches[i].x > leftButtonX && touches[i].x < leftButtonX + buttonWidth)&&(touches[i].y > buttonY && touches[i].y < buttonY + buttonWidth)) {
-      leftBarWidth = leftBarWidth + 2
-    }
-    if ((touches[i].x > rightButtonX && touches[i].x < rightButtonX + buttonWidth)&&(touches[i].y > buttonY && touches[i].y < buttonY + buttonWidth)) {
-      rightButtonBar.visible = true
-      rightBarWidth = rightBarWidth + 2
+  // ellipse(56, 46, 55, 55);
 
+  //     // leftBarWidth = leftBarWidth + 2
+  //   } else if ((touches[i].x > rightButtonX && touches[i].x < rightButtonX + buttonWidth)&&(touches[i].y > buttonY && touches[i].y < buttonY + buttonWidth)) {
+  //     text("touched right", width/2, height/2 + 20);
+  //     // rightButtonBar.visible = true
+  //     // rightBarWidth = rightBarWidth + 2
+  //   }
+  // }
+}
+
+function touchStarted() {
+
+  text("Number of touches: " + touches.length, 200,200);
+
+  if(touches.length >= 2) {
+
+    var t0 = touches[0];
+    var t1 = touches[1];
+
+    var t0WithinLeft = withinLeft(t0);
+    var t0WithinRight = withinRight(t0);
+
+    var t1WithinLeft = withinLeft(t1);
+    var t1WithinRight = withinRight(t1);
+
+    if(t0WithinLeft && t1WithinRight) {
+      text("two touches", 100,100);
+    } else if(t0WithinRight && t1WithinLeft) {
+      text("two touches", 100,100);
+    } else {
+      text("how many touches: " + touches.length, 200,100);
     }
-    text("touched", width/2, height/2);
+  }
+  return false;
+}
+
+// function mousePressed() {
+//   console.log(mouseX, mouseY);
+//   if (((mouseX > leftButtonX && mouseX < leftButtonX + buttonWidth)&&(mouseY > leftButtonY && mouseY < leftButtonY + buttonWidth))&&((mouseX > rightButtonX && mouseX < rightButtonX + buttonWidth)&&(mouseY > rightButtonY && mouseY < rightButtonY + buttonWidth))) {
+//     console.log("both button pressed");
+//   } else if ((mouseX > leftButtonX && mouseX < leftButtonX + buttonWidth)&&(mouseY > leftButtonY && mouseY < leftButtonY + buttonWidth)) {
+//     console.log("button pressed");
+//     //text("two fingers", 100, 100);
+//   } else if ((mouseX > rightButtonX && mouseX < rightButtonX + buttonWidth)&&(mouseY > rightButtonY && mouseY < rightButtonY + buttonWidth)) {
+//     console.log("r button pressed");
+//   }
+// }
+
+// this prevents dragging screen around
+// function touchMoved() {
+//   return false;
+// }
+
 
 
 // if left bar width is greater than half of the distance between the buttons
     // One color per finger
     // fill(colors[i]);
     // Draw a circle at each finger
-  }
-
-}
-
-// When the user clicks the mouse
-function mousePressed() {
-  // Check if mouse is inside the circle
-  var d = dist(mouseX, mouseY, 360, 200);
-  if (d < 100) {
-    // Pick new random color values
-    r = random(255);
-    g = random(255);
-    b = random(255);
-  }
-}
-
-  // drawSprites();
-
-// this prevents dragging screen around
-function touchMoved() {
-  return false;
-}
